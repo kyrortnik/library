c7f6c7f28277ac6b06a36775fc37585fbec6da6e
Mikhail Piatrenka <kyrortnik97@gmail.com>
2021-10-08 20:50:38 +0300

    dao update

--- dao/src/main/java/repository/ConnectionPool.java
+++ dao/src/main/java/repository/ConnectionPool.java
@@ -6,11 +6,12 @@ import java.util.Properties;
 
 public interface ConnectionPool {
 
-   /* Connection provide();
-
-    Connection release();*/
-
     Connection getConnection() throws SQLException;
+
+
+    boolean releaseConnection(Connection connection);
+
+
 
 
 }
--- dao/src/main/java/repository/ConnectionPoolImpl.java
+++ dao/src/main/java/repository/ConnectionPoolImpl.java
@@ -4,77 +4,52 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Properties;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingDeque;
+
 
 public class ConnectionPoolImpl implements ConnectionPool {
 
-   private ArrayList<Connection> availableConnections;
-   private ArrayList<Connection> takenConnections;
+   private final ArrayList<Connection> availableConnections;
+   private final ArrayList<Connection> takenConnections = new ArrayList<>();
    private static final int INITIAL_POOL_SIZE = 10;
    private static final int MAX_POOL_SIZE = 20;
    private static final int MAX_TIMEOUT = 5;
-
-   PropertyInitializer propertyInitializer = new PropertyInitializer();
-
-   private Properties properties = propertyInitializer.getProperties();
-
-   private  final String URL = propertyInitializer.getProperty("url");
-   private  final String USER = propertyInitializer.getProperty("user");
-   private  final String PASSWORD = propertyInitializer.getProperty("password");
-   private  final String DATABASE_NAME = propertyInitializer.getProperty("database_name");
+    private final String url;
+    private final String username;
+    private final String password;
 
 
 
-   public static ConnectionPoolImpl create(String url,String user,String password) throws SQLException {
-       ArrayList<Connection> pool = new ArrayList<>(INITIAL_POOL_SIZE);
-       for (int i = 0;i < INITIAL_POOL_SIZE;i++){
-           pool.add(createConnection(url,user,password));
-       }
-       return new ConnectionPoolImpl(/*propertyInitializer,pool*/);
+    public static ConnectionPoolImpl create(String url,String username,String password) throws SQLException {
 
+        ArrayList<Connection> connectionPool = new ArrayList<>(INITIAL_POOL_SIZE);
+        for (int i = 0;i < INITIAL_POOL_SIZE;i++){
+            connectionPool.add(createConnection(url,username,password));
+        }
 
-   }
+        return new ConnectionPoolImpl(url,username,password,connectionPool);
 
-    private ConnectionPoolImpl(/*PropertyInitializer propertyInitializer, ArrayList<Connection> availableConnections*/) {
-
-      /* URL = propertyInitializer.getProperty("url");
-       USER = propertyInitializer.getProperty("user");
-       PASSWORD = propertyInitializer.getProperty("password");
-       DATABASE_NAME = propertyInitializer.getProperty("database_name");*/
 
     }
 
-    /*@Override
-    public Connection getConnection(Properties credentials) {
-        String url = credentials.getProperty("URL");
-        String user = credentials.getProperty("USER");
-        String password = credentials.getProperty("PASSWORD");
-        String dbName = credentials.getProperty("DATABASE_NAME");
 
+    private ConnectionPoolImpl(String url,String username,String password, ArrayList<Connection> availableConnections) {
+         this.url = url;
+         this.username = username;
+         this.password = password;
+         this.availableConnections = availableConnections;
+    }
 
-    }*/
 
     private static Connection createConnection(String url,String user,String password) throws SQLException {
         return DriverManager.getConnection(url, user, password);
     }
 
-    /* @Override
-    public Connection provide() {
-        return null;
-    }
-
-    @Override
-    public Connection release() {
-        return null;
-    }*/
-
     @Override
     public Connection getConnection() throws SQLException {
+
         if (availableConnections.isEmpty()) {
             if (takenConnections.size() < MAX_POOL_SIZE) {
-                availableConnections.add(createConnection(URL, USER, PASSWORD));
+                availableConnections.add(createConnection(url, username, password));
             } else {
                 throw new RuntimeException("Maximum pool size reached, no available connections!");
             }
@@ -83,13 +58,18 @@ public class ConnectionPoolImpl implements ConnectionPool {
         Connection connection = availableConnections.remove(availableConnections.size() - 1);
 
         if(!connection.isValid(MAX_TIMEOUT)){
-            connection = createConnection(URL, USER, PASSWORD);
+            connection = createConnection(url, username, password);
         }
 
         takenConnections.add(connection);
         return connection;
     }
 
+    @Override
+    public boolean releaseConnection(Connection connection) {
+        availableConnections.add(connection);
+        return takenConnections.remove(connection);
+    }
 
 
 }
